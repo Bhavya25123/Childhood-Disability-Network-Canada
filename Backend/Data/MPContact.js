9a3fb10163a407b99312049a73c046e66fc49c5e
Bhavya Shah <bhavyashah2503@gmail.com>
2025-09-10 12:03:08 -0400

    Feat: login and findMp working

--- Backend/Data/MPContact.js
+++ Backend/Data/MPContact.js
@@ -1,22 +1,13 @@
 const mongoose = require("mongoose");
 
 const mpContactSchema = new mongoose.Schema(
-  {
-    "First Name": String,
-    "Last Name": String,
-    Constituency: String,
-    "Province / Territory": String,
-    "Political Affiliation": String,
-    "Start Date": String,
-  },
-  { strict: false }
-);
+ { "Honorific Title": String,
+  "First Name": String,
+  "Last Name": String,
+  "Constituency": String,
+  "Province / Territory": String,
+  "Political Affiliation": String,
+  "Start Date": String,
+}, { collection: "MPContacts" });
 
-// Reuse the main connection string if a dedicated MP one isn't provided
-const mpUri = process.env.MP_MONGO_URI || process.env.MONGO_URI;
-
-const mpConnection = mongoose.createConnection(mpUri, {
-  dbName: process.env.MP_DB_NAME || "MPContacts",
-});
-
-module.exports = mpConnection.model("MPContact", mpContactSchema);
+module.exports = mongoose.model("MPContact", mpContactSchema);
